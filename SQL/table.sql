92ab7c3712d254f3ee55a76c534523ab58222bdb
Abraham youkhanna <abrahamy3322@gmail.com>
2021-04-26 14:42:51 -0500

    created sql table

---
+++ SQL/table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE videogame_sales(
+	name VARCHAR,
+	platform VARCHAR,
+	year_of_release FLOAT,
+	genre VARCHAR,
+	publisher VARCHAR,
+	na_sales FLOAT,
+	eu_sales FLOAT,
+	jp_sales FLOAT,
+	other_sales FLOAT,
+	global_sales FLOAT, 
+	critic_score FLOAT,
+	critic_count FLOAT,
+	user_score FLOAT, 
+	user_count INT,
+	developer VARCHAR,
+	rating VARCHAR
+);
